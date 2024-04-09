4bed9d0d8dfdb9f540e5d97b6ed39900e0534ee1
Qianxi Chen <qc815@nyu.edu>
2024-04-09 15:16:18 -0400

    frontend - switch to desktop application with electron-reactjs

---
+++ frontend/src/renderer/pages/LoginPage.js
@@ -0,0 +1,11 @@
+import styles from "./LoginPage.module.css";
+
+const LoginPage = () => {
+    return (
+        <div className="bg single-center">
+            <div className={styles.login}></div>
+        </div>
+    );
+};
+
+export default LoginPage;
